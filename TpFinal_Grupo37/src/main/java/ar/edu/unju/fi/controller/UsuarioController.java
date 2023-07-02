352a5ba67b23b036a98653bd0e04974fabfa8a66
daniiestefii <dani.estefania9304@gmail.com>
2023-07-02 18:26:27 -0300

    Refactor controller usuario

--- TpFinal_Grupo37/src/main/java/ar/edu/unju/fi/controller/UsuarioController.java
+++ TpFinal_Grupo37/src/main/java/ar/edu/unju/fi/controller/UsuarioController.java
@@ -1,6 +1,4 @@
 package ar.edu.unju.fi.controller;
-import ar.edu.unju.fi.entity.Usuario;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
