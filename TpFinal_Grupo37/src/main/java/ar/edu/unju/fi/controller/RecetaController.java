84d2cacd064cd136217453de10ad20708c6e40ea
daniiestefii <dani.estefania9304@gmail.com>
2023-07-08 16:55:36 -0300

    Se agrego documentacion receta

--- TpFinal_Grupo37/src/main/java/ar/edu/unju/fi/controller/RecetaController.java
+++ TpFinal_Grupo37/src/main/java/ar/edu/unju/fi/controller/RecetaController.java
@@ -23,6 +23,15 @@ import ar.edu.unju.fi.service.IRecetaService;
 import ar.edu.unju.fi.service.IUsuarioService;
 import jakarta.validation.Valid;
 
+/**
+ @author Chosco
+ @author Delgado
+ @author Mullicundo
+ @author Ordonez
+ @author Villalba
+ @version 17
+ */
+
 @Controller
 @RequestMapping("/recetas")
 public class RecetaController {
@@ -38,13 +47,25 @@ public class RecetaController {
 	
 	@Autowired
 	private Usuario usuario;
-	
+
+	/**
+	 * Muestra la lista de recetas.
+	 * @param model el modelo utilizado para pasar atributos a la vista
+	 * @return la vista "control"
+	 */
 	@GetMapping("/acceso")
 	public String contraseñaAcesso(Model model) {
 		model.addAttribute("recetas", true);
 		return "control";
 	}
-	
+
+	/**
+	 * Verificar si el usuario existe y obtenerlo
+	 *  Si el usuario tiene el rol correcto, redirigir a la lista de recetas,sino
+	 *  el usuario no tiene el rol correcto, mostrar mensaje de error
+	 *  @param model utilizado para pasar la lista a la vista.
+	 *  @return String.
+	 */
 	@PostMapping("/control")
 	public String contraseñaControl(Model model, @RequestParam("codigo") String codigo) {
 
@@ -64,26 +85,45 @@ public class RecetaController {
 			return "control";
 		}
 	}
-	
+
+	/**
+	 * Muestra la vista de categorías de recetas.
+	 * @param model el modelo utilizado para pasar atributos a la vista
+	 * @return la vista "categorias_recetas" que muestra la lista de categorías de recetas
+	 */
 	@GetMapping("/categoria")
 	public String vistaCategorias(Model model) {
 		model.addAttribute("categorias", recetaService.getCategoriasUnicas(recetaService.getListaRecetas()));
 		return "categorias_recetas";
 	}
-	
-	
+
+	/**
+	 * Muestra la lista de recetas.
+	 * @param model utilizado para pasar la lista a la vista.
+	 * @return vista "recetas" que muestra la lista de recetas.
+	 */
 	@GetMapping("/vista")
 	public String listaRecetas(Model model) {
 		model.addAttribute("recetas", recetaService.getListaRecetas());
 		return "receta";
 	}
-	
+
+	/**
+	 * Muestra la lista de recetas.
+	 * @param model utilizado para pasar la lista a la vista.
+	 * @return vista "recetas" que muestra la lista de recetas.
+	 */
 	@GetMapping("/listado")
 	public String mostrarRecetas(Model model) {
 		model.addAttribute("recetas", recetaService.getListaRecetas());
 	    return "recetas";
 	}
-	
+
+	/**
+	 * Muestra el formulario para crear una nuevo receta.
+	 * @param model utilizado para pasar la nuevo receta a la vista.
+	 * @return vista "nuevo_receta" que muestra el formulario para crear una nuevo receta.
+	 */
 	@GetMapping("/nueva_receta")
 	public String getNuevaReceta(Model model) {
 	    boolean edicion = false;	    
@@ -94,7 +134,13 @@ public class RecetaController {
 	    
 	    return "nueva_receta";
 	}
-	
+
+	/**
+	 * Guarda una nuevo receta.
+	 * @param objeto de tipo receta a guardar.
+	 * @param result objeto BindingResult que contiene los resultados de la validación.
+	 * @return modelo y vista "recetas" que muestra la lista actualizada de las recetas.
+	 */
 	@PostMapping("/guardar_receta")
 	public ModelAndView getGuardarReceta(@Valid @ModelAttribute("receta") Receta receta, BindingResult result) {
 		
@@ -111,7 +157,13 @@ public class RecetaController {
 		modelView.addObject("recetas", recetaService.getListaRecetas());
 		return modelView;
 	}
-	
+
+	/**
+	 * Muestra el formulario para modificar una receta existente.
+	 * @param model utilizado para pasar la receta a modificar y los ingredientes a la vista.
+	 * @param id  de la receta a modificar.
+	 * @return vista "nueva_receta" que muestra el formulario para modificar una receta existente.
+	 */
 	
 	@GetMapping("/modificar/{id}")
 	public String getModificarRecetaPage(Model model, @PathVariable(value="id") Long id) {
@@ -123,7 +175,13 @@ public class RecetaController {
 		return "nueva_receta";
 	}
 
-	
+	/**
+	 * Modifica una receta existente.
+	 * @param receta objeto de tipo Receta a modificar.
+	 * @param result objeto BindingResult que contiene los resultados de la validación.
+	 * @param model utilizado para pasar la receta y los ingredientes a la vista.
+	 * @return redirección a la lista de recetas si la modificación es exitosa.
+	 */
 	@PostMapping("/modificar_receta")
 	public String modificarReceta(@Valid @ModelAttribute("receta") Receta receta,BindingResult result, Model model) {
 		if(result.hasErrors()) {
@@ -136,13 +194,24 @@ public class RecetaController {
 		recetaService.modificarReceta(receta);
 		return "redirect:/recetas/listado";
 	}
-	
+
+	/**
+	 * Elimina una receta.
+	 * @param id  de la receta a eliminar.
+	 * @return redirección a la lista de recetas.
+	 */
 	@GetMapping("/eliminar/{id}")
 	public String eliminarReceta(@PathVariable(value="id")Long id) {
 		recetaService.eliminarReceta(recetaService.findRecetaById(id));
 		return "redirect:/recetas/listado";
 	}
-	
+
+	/**
+	 * Muestra los detalles de una receta.
+	 * @param model utilizado para pasar la receta y los ingredientes a la vista.
+	 * @param id  de la receta a mostrar los detalles.
+	 * @return vista "receta_detalle" que muestra los detalles de la receta.
+	 */
 	@GetMapping("/detalle/{id}")
 	public String geDetalleRecetaPage(Model model, @PathVariable(value="id") Long id) {
 
@@ -152,7 +221,13 @@ public class RecetaController {
 		model.addAttribute("ingredientes", receta.getIngredientes());
 		return "receta_detalle";
 	}
-	
+
+	/**
+	 * Filtra las recetas por categoría.
+	 * @param model utilizado para pasar las recetas filtradas a la vista.
+	 * @param categoria Categoría por la cual se van a filtrar las recetas.
+	 * @return vista "receta" que muestra las recetas filtradas.
+	 */
 	@GetMapping("/filtro/{categoria}")
 	public String getfiltoServicioCorte(Model model, @PathVariable(value="categoria")String categoria) {
 		
@@ -161,6 +236,13 @@ public class RecetaController {
 
 		return "receta";
 	}
+
+	/**
+	 * Muestra las recetas por categoría.
+	 * @param model utilizado para pasar las recetas y la categoría a la vista.
+	 * @param categoria Categoría por la cual se van a filtrar las recetas.
+	 * @return vista "recetas_por_categoria" que muestra las recetas por categoría.
+	 */
 	@GetMapping("/categoria/{categoria}")
 	public String getRecetasPorCategoria(Model model, @PathVariable String categoria) {
 	    // Obtener la lista de recetas por categoría
--- TpFinal_Grupo37/src/main/java/ar/edu/unju/fi/service/imp/RecetaServiceImp.java
+++ TpFinal_Grupo37/src/main/java/ar/edu/unju/fi/service/imp/RecetaServiceImp.java
@@ -14,12 +14,26 @@ import ar.edu.unju.fi.entity.Receta;
 import ar.edu.unju.fi.repository.IRecetaRepository;
 import ar.edu.unju.fi.service.IRecetaService;
 
+/**
+ @author Chosco
+ @author Delgado
+ @author Mullicundo
+ @author Ordoñez
+ @author Villalba
+ @version 17
+ */
+
+
 @Service
 public class RecetaServiceImp implements IRecetaService{
 
 	@Autowired
 	private IRecetaRepository recetaRepository;
-	
+
+	/**
+	 * Guarda una receta en la base de datos.
+	 * @param receta la receta a guardar.
+	 */
 	@Override
 	public void guardarReceta(Receta receta) {
 		receta.setEstado(true);
@@ -30,6 +44,10 @@ public class RecetaServiceImp implements IRecetaService{
 		recetaRepository.save(receta);
 	}
 
+	/**
+	 * Obtiene una lista de recetas de la base de datos.
+	 * @return una lista de recetas.
+	 */
 	@Override
     public List<Receta> getListaRecetas() {
         List<Receta> recetas =recetaRepository.findByEstado(true);
@@ -39,17 +57,32 @@ public class RecetaServiceImp implements IRecetaService{
         return recetas ;
     }
 
+	/**
+	 * Busca una receta en la base de datos por su id.
+	 * @param id el id de la receta a buscar.
+	 * @return la receta encontrada.
+	 */
 	@Override
 	public Receta findRecetaById(Long id) {
-		return recetaRepository.findById(id).get();
+		Receta receta=recetaRepository.findById(id).get();
+		receta.setListaPreparaciones(generarLista(receta.getPreparacion()));
+		return receta;
 	}
 
+	/**
+	 * Elimina una receta de la base de datos.
+	 * @param receta la receta a eliminar.
+	 */
 	@Override
 	public void eliminarReceta(Receta receta) {
 		receta.setEstado(false);
 		recetaRepository.save(receta);
 	}
 
+	/**
+	 * Modifica una receta en la base de datos.
+	 * @param receta la receta a modificar.
+	 */
 	@Override
 	public void modificarReceta(Receta receta) {
 		receta.setEstado(true);
@@ -61,17 +94,31 @@ public class RecetaServiceImp implements IRecetaService{
 		recetaRepository.save(receta);
 	}
 
+	/**
+	 * Crea una nueva receta.
+	 * @return la nueva receta creada.
+	 */
 	@Override
 	public Receta nuevaReceta() {
 		
 		return new Receta();
 	}
 
+	/**
+	 * Filtra las recetas de la base de datos por categoría.
+	 * @param categoria la categoría por la que filtrar las recetas.
+	 * @return una lista de recetas filtradas por categoría.
+	 */
 	@Override
 	public List<Receta> filtroRecetaCategoria(String categoria) {
 		return recetaRepository.findByCategoriaAndEstado(categoria, true);
 	}
 
+	/**
+	 * Genera una lista de subcadenas a partir de una cadena dada.
+	 * @param cadena la cadena a dividir en subcadenas.
+	 * @return una lista de subcadenas.
+	 */
 	@Override
     public List<String> generarLista(String cadena) {
         String[] subcadenas = cadena.split("\\.");
@@ -79,6 +126,11 @@ public class RecetaServiceImp implements IRecetaService{
         return listaSubcadenas;
     }
 
+	/**
+	 * Obtiene un conjunto de categorías únicas a partir de una lista de recetas.
+	 * @param recetas la lista de recetas de donde se obtendrán las categorías únicas
+	 * @return un conjunto de categorías únicas obtenidas de la lista de recetas
+	 */
 	@Override
 	public Set<String> getCategoriasUnicas(List<Receta> recetas) {
 		Set<String> categorias = new HashSet<>();
